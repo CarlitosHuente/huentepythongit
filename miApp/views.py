f8f7d90018a2849273b4eb55a8abc17d10d17736
CarlitosHuente <carloscarvajal2.0@gmail.com>
2024-02-27 20:50:29 -0300

    segunda actualizacion de la web

--- miApp/views.py
+++ miApp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('¡Hola, mundo! Carlitos')
+    return HttpResponse('¡Hola, mundo! Carlitos prueba segunda')
